e624a03cd4d9c95db727db9001c9317cca1ee810
Warden <samer.charaf3@gmail.com>
2023-01-08 20:19:34 +0300

    .

--- Assets/Scripts/Weapon.gd
+++ Assets/Scripts/Weapon.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
-func _ready():
-	pass # Replace with function body.
-
-
 func _physics_process(delta):
 	position += transform.x * 7;
+	$Area2D.connect("body_entered",self,"_on_enter")
+	
+func _on_enter(_area2d):
+	self.queue_free()
