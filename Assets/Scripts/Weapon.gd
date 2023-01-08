9df441f14ba9b5a449070dcd7733bdfad73ee5eb
HDZD <hadizein66@gmail.com>
2023-01-08 23:18:15 +0300

    fix warning

--- Assets/Scripts/Weapon.gd
+++ Assets/Scripts/Weapon.gd
@@ -3,7 +3,7 @@ extends Node2D
 func _ready():
 	$Area2D.connect("body_entered",self,"_on_enter")
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	position += transform.x * 7;
 
 func _on_enter(_area2d):
